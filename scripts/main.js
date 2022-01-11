97076ca2ba90cb4d27c31d316029a7092401ee7a
David Clark <david.clark.1043@gmail.com>
2022-01-11 09:19:32 -0600

    Added spinning cursor

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,34 @@ yesBtn.onclick = function() {
 noBtn.onclick = function() {
     cookieBox.style.display = "block";
 }
+
+
+const tooltip = new (function () {
+    const node = document.createElement('img');
+    node.className = 'tooltip';
+    node.setAttribute('hidden', '');
+    node.setAttribute('src', 'images/hotdogman.gif')
+    document.body.appendChild(node);
+
+    this.follow = function (event) {
+        node.style.left = event.clientX + window.scrollX + 'px';
+        node.style.top = event.clientY + window.scrollY + 'px';
+    };
+
+    this.show = function (event) {
+        node.textContent = event.target.dataset.tooltip;
+        node.removeAttribute('hidden');
+    };
+
+    this.hide = function () {
+        node.setAttribute('hidden', '');
+    };
+})();
+
+const links = document.querySelectorAll('body');
+
+links.forEach(link => {
+    link.onmouseover = tooltip.show;
+    link.onmousemove = tooltip.follow;
+    // link.onmouseout = tooltip.hide;
+});
